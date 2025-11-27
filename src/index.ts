58915b22ad128e852a282b481b89bfd6dbf0d325
MattisJensen <matjen@mail.de>
2025-11-27 17:19:29 +0100

    server setup

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { createExpressApp } from './app.js';
+import { DependencyProvider } from './dependencyProvider.js';
+
+const port = process.env.PORT || 3000;
+
+// Dependency Injection via provider
+const dependencies = new DependencyProvider();
+
+// Create Express app
+const app = createExpressApp(
+  dependencies.checkDatabaseConnectionUseCase,
+  dependencies.checkDataAvailabilityUseCase
+);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Health Check API running on port ${port}`);
+});
