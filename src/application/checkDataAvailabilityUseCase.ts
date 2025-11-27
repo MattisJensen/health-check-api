fcd5ed608c5137f3cd2ea71f9853136db859a269
MattisJensen <matjen@mail.de>
2025-11-27 16:07:03 +0100

    add CheckDataAvailabilityUseCase

---
+++ src/application/checkDataAvailabilityUseCase.ts
@@ -0,0 +1,20 @@
+import type { HealthCheckRepository } from "./healthCheckRepository.js";
+import { DatabaseConnectionError } from "../core/databaseConnectionError.js";
+
+export class CheckDataAvailabilityUseCase {
+  private repository: HealthCheckRepository;
+
+  constructor(repository: HealthCheckRepository) {
+    this.repository = repository;
+  }
+
+  async execute(): Promise<boolean> {
+    try {
+      return await this.repository.hasRequiredData();
+    } catch {
+      throw new DatabaseConnectionError(
+        "Failed to check required data availability"
+      );
+    }
+  }
+}
